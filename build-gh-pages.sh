4794aa293d89a7aa790507419ff86d5903b19822
Tobias Bosch <tobias.bosch@opitz-consulting.com>
2011-07-31 21:32:19 +0200

    third try

--- build-gh-pages.sh
+++ build-gh-pages.sh
@@ -1,13 +1,16 @@
 # Build script to update the branch gh-pages, which
 # contains the live copy of the demo.
+TMP=/tmp/gh-pages
+SRC=target/requirejs/output
 if $M2_HOME/bin/mvn package; then
-    mkdir /tmp/gh-pages
-    rmdir -fr /tmp/gh-pages
-    cp -r target/requirejs/output/ /tmp/gh-pages
+    mkdir $TMP
+    rmdir -fr $TMP
+    cp -r $SRC/ $TMP
+	find $TMP -name "*.js" ! -name "main*.js" ! -name "require.js" -exec rm "{}" ";"    
     if git checkout gh-pages; then
       rm -fr *
-      cp -r /tmp/gh-pages/ .
-      rmdir -fr /tmp/gh-pages
+      cp -r $TMP/ .
+      rmdir -fr $TMP
       #git add .
       #git commit -m "updated pages"
       #git checkout master
