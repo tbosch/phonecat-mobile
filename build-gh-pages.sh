8c0fc2f806ca93109f9b86af0de0dbb5b1be6042
Tobias Bosch <tobias.bosch@opitz-consulting.com>
2011-07-31 21:51:13 +0200

    changed gh-pages build script

--- build-gh-pages.sh
+++ build-gh-pages.sh
@@ -11,9 +11,9 @@ if $M2_HOME/bin/mvn package; then
     if git checkout gh-pages; then
       rm -fr *
       cp -r $TMP/ .
-      git add .
-      git commit -a -m "updated pages"
-      git checkout master
+      #git add .
+      #git commit -a -m "updated pages"
+      #git checkout master
     fi
 fi
 
