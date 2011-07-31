14b1c9c425ecff0ed90a8e7013b68e9376b73e70
Tobias Bosch <tobias.bosch@opitz-consulting.com>
2011-07-31 11:35:47 +0200

    jstd ui tests also running, and mvn integration-test -Pintegration works well

--- jstd-ui.sh
+++ jstd-ui.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-mvn exec:java -Dexec.classpathScope=test -Dexec.mainClass=com.google.jstestdriver.JsTestDriver -Dexec.args="--config jstd-ui.conf --reset --tests all --testOutput target/jstd-reports"
+$M2_HOME/bin/mvn exec:java -Dexec.classpathScope=test -Dexec.mainClass=com.google.jstestdriver.JsTestDriver -Dexec.args="--config jstd-ui.conf --reset --tests all --testOutput target/jstd-reports"
