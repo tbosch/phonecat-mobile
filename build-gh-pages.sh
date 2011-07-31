cad8a748ee40ac300d18df7298f2f6f9513493c6
Tobias Bosch <tobias.bosch@opitz-consulting.com>
2011-07-31 21:39:39 +0200

    5th try

--- build-gh-pages.sh
+++ build-gh-pages.sh
@@ -3,14 +3,15 @@
 TMP=/tmp/gh-pages
 SRC=target/requirejs/output
 if $M2_HOME/bin/mvn package; then
-    mkdir $TMP
-    rmdir -fr $TMP
+    mkdir -p $TMP
+    rm -fr $TMP
     cp -r $SRC/ $TMP
 	find $TMP -name "*.js" ! -name "main*.js" ! -name "require.js" -exec rm "{}" ";"    
+    find $TMP -depth -type d -empty -exec rmdir "{}" ";"	
     if git checkout gh-pages; then
       rm -fr *
       cp -r $TMP/ .
-      rmdir -fr $TMP
+      rm -fr $TMP
       git add .
       git commit -a -m "updated pages"
       git checkout master
