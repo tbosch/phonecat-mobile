f8a4ff2b597e7bc89feb163a7727bab592a20db2
Tobias Bosch <tobias.bosch@opitz-consulting.com>
2011-07-31 21:23:54 +0200

    second try

--- build-gh-pages.sh
+++ build-gh-pages.sh
@@ -1,9 +1,13 @@
 # Build script to update the branch gh-pages, which
 # contains the live copy of the demo.
 if $M2_HOME/bin/mvn package; then
+    mkdir /tmp/gh-pages
+    rmdir -fr /tmp/gh-pages
+    cp -r target/requirejs/output/ /tmp/gh-pages
     if git checkout gh-pages; then
       rm -fr *
-      cp -r target/requirejs/output/ .
+      cp -r /tmp/gh-pages/ .
+      rmdir -fr /tmp/gh-pages
       #git add .
       #git commit -m "updated pages"
       #git checkout master
