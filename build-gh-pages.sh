fb689d875a15e8178f6ab78a35e15a5164dfb727
Tobias Bosch <tobias.bosch@opitz-consulting.com>
2012-03-12 09:53:16 +0100

    fixed gh-pages build

--- build-gh-pages.sh
+++ build-gh-pages.sh
@@ -2,11 +2,10 @@
 # contains the live copy of the demo.
 TMP=/tmp/gh-pages
 SRC=target/requirejs/build
-if $M2_HOME/bin/mvn package; then
+if mvn package; then
     mkdir -p $TMP
     rm -fr $TMP
     cp -r $SRC/ $TMP
-	find $TMP -name "*.js" ! -name "main*.js" ! -name "require.js" -exec rm "{}" ";"    
     find $TMP -depth -type d -empty -exec rmdir "{}" ";"
     git stash
     if git checkout gh-pages; then
